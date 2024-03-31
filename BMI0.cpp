1fba356077e4179ae3cf44a6d7a83c8f8a8506cd
AndrewHoemke <89616312+AndrewHoemke@users.noreply.github.com>
2024-03-31 13:57:54 -0500

    Add files via upload
    Removed Console UI, now uses command line arguments. If no command line arguments are given, run tests.

--- BMI0.cpp
+++ BMI0.cpp
@@ -9,52 +9,6 @@
 #include <conio.h>
 
 
-int line = 0;
-//Aquired function from stackoverflow. Changed function and variable names to better match my intent.
-void SetPosition(int x, int y)
-{
-    COORD pos = {x, y};
-    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(output, pos);
-}
-
-void ClearLine(int length)
-{
-    for (int i = 0; i < length; i = i + 1)
-    {
-        std::cout << ' ';
-    }
-}
-
-int FeetToInches(int feet)
-{
-    return 12 * feet;
-}
-
-float InchesToMeters(int inches)
-{
-    return 0.025 * static_cast<float>(inches);
-}
-
-float PoundsToKilograms(int pounds)
-{
-    return 0.45 * static_cast<float>(pounds);
-}
-
-int ConvertInput(std::string input)
-{
-    //If any non-numeral characters are found, return -1.
-    for (int i = 0; i < input.length(); i = i + 1)
-    {
-        if (input.at(i) < 48 || input.at(i) > 57)
-        {
-            return -1;
-        }
-    }
-    //cast to integer
-    return std::stoi(input);
-}
-
 std::string InterpretBMI(float bmi)
 {
     if (bmi < 18.5)
@@ -75,160 +29,76 @@ std::string InterpretBMI(float bmi)
     }
 }
 
-float CalculateBMI(float height, float weight)
-{
-    return (weight)/(height * height);
-}
 
-void BMICalculator()
+float BMICalculator(int feet, int inches, int pounds)
 {
-    std::string input;//typed input, before checks.
-    int inches = 0;
-    int feet = 0;
-    int pounds = 0;
+    int input;
     float height = 0;//meters
     float weight = 0; //kilograms
     float bmi = 0;
-    line = 0;
-    system("cls");
-    std::cout << "Please answer the following questions with whole numbers only:";
-    line = line + 1;
-    SetPosition(0, line);
-    std::cout << "How tall are you in feet?";
-    line = line + 1;
-    SetPosition(0, line);
-    std::cin >> input;
-    line = line + 1;
-    SetPosition(0, line);
-    while (ConvertInput(input) == -1)
-    {
-        //clear line and line before
-        ClearLine(input.length());
-        line = line - 1;
-        SetPosition(0, line);
-        std::cout << "Please enter a whole number";
-        line = line + 1;
-        SetPosition(0, line);
-        std::cin >> input;
-        line = line + 1;
-        SetPosition(0, line);
-        if (ConvertInput(input) == -1)
-        {
-            line = line - 1;
-            SetPosition(0, line);
-        }
-    }
-    feet = ConvertInput(input);
     //inches = FeetToInches(feet);
-
-    std::cout << "How many inches taller are you than that?";
-    line = line + 1;
-    SetPosition(0, line);
-    std::cin >> input;
-    line = line + 1;
-    SetPosition(0, line);
-    while (ConvertInput(input) == -1)
-    {
-        //clear line and line before
-        ClearLine(input.length());
-        line = line - 1;
-        SetPosition(0, line);
-        std::cout << "Please enter a whole number";
-        line = line + 1;
-        SetPosition(0, line);
-        std::cin >> input;
-        line = line + 1;
-        SetPosition(0, line);
-        if (ConvertInput(input) == -1)
-        {
-            line = line - 1;
-            SetPosition(0, line);
-        }
-    }
-    inches = ConvertInput(input);
-    height = InchesToMeters(inches+FeetToInches(feet));
-
-    std::cout << "How much do you weigh in pounds?";
-    line = line + 1;
-    SetPosition(0, line);
-    std::cin >> input;
-    line = line + 1;
-    SetPosition(0, line);
-    while (ConvertInput(input) == -1)
-    {
-        //clear line and line before
-        ClearLine(input.length());
-        line = line - 1;
-        SetPosition(0, line);
-        std::cout << "Please enter a whole number";
-        line = line + 1;
-        SetPosition(0, line);
-        std::cin >> input;
-        line = line + 1;
-        SetPosition(0, line);
-        if (ConvertInput(input) == -1)
-        {
-            line = line - 1;
-            SetPosition(0, line);
-        }
-    }
-    
-    pounds = ConvertInput(input);
-    weight = PoundsToKilograms(pounds);
-    bmi = CalculateBMI(height, weight);
-    std::cout << "Height: " << feet << "'" << " " << inches << (char)34 << "\nWeight: " << pounds << "lbs";
-    line = line + 2;
-    SetPosition(0, line);
-    printf("Your BMI is: %.1f", bmi);
-    line = line + 1;
-    SetPosition(0, line);
-    std::cout << "you are " << InterpretBMI(bmi);
-    line = line + 1;
-    SetPosition(0, line);
-    std::cout << "Enter any letter to return to the menu";
-    line = line + 1;
-    SetPosition(0,line);
-    std::cin >> input;
-
+    height = 0.025 * static_cast<float>(inches+(12*feet));
+    weight = 0.45 * static_cast<float>(pounds);
+    bmi = (weight) / (height * height);
+    return bmi;
 }
 
-
-void MenuSystem()
+int TestBMI()
 {
-    std::string input;
-    bool badCmd = false;
-    while (1)
+    //8 tests.
+    int failedTests = 0;
+    int tests[8][3] = 
     {
-        system("cls");
-        line = 0;
-        if (badCmd)
+        {6, 0, 216},
+        {6, 0, 215},
+        {6, 5, 215},
+        {6, 0, 180},
+        {6, 0, 179},
+        {6, 8, 180},
+        {5, 3, 102},
+        {5, 4, 105}
+    };
+    std::string expVals[8] = {"obese","overweight","overweight","overweight","normal weight","normal weight","normal weight","underweight"};
+    for (int i = 0; i < 8; i = i + 1)
+    {
+        if (InterpretBMI(BMICalculator(tests[i][0], tests[i][1], tests[i][2])) != expVals[i])
         {
-            std::cout << "Command not understood. ";
-            badCmd = false;
+            std::cout << "Test " << i << " failed\n";
+            failedTests = failedTests + 1;
         }
-        std::cout << "Please select one of the listed functions. Type 'exit' to exit the program. \n1: bmi\n";
-        line = line + 2;
-        std::cin >> input;
-        line = line + 1;
-        //only one function right now
-        if (input == "1" || input == "bmi" || input == "BMI" || input == "Bmi")
+    }
+    return failedTests;
+}
+
+int main(int argc, char* argv[])
+{
+    //std::cout << "1: " <<argv[1] << "\n";
+    //std::cout << "2: " << argv[2] << "\n";
+    //std::cout << "3: " << argv[3] << "\n";
+    //argv[1] is height in feet
+    //argv[2] is height in inches
+    //argv[3] is weight in pounds
+    //return bmi value
+    //if argc is 1 (program run with no arguments) begin testing.
+    //std::cout << "argc is " << argc;
+    if (argc == 1)
+    {
+        int failedTests = TestBMI();
+        if (failedTests == 0)
         {
-            BMICalculator();
-        }
-        else if (input == "exit" || input == "Exit" || input == "EXIT")
-        {
-            return;
+            std::cout << "All tests passed";
         }
         else
         {
-            badCmd = true;
+            std::cout << failedTests << " tests failed";
         }
+        
+    }
+    else
+    {
+        std::cout << "You are " << BMICalculator(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])) << "\n";
+        std::cout << "You are " << InterpretBMI(BMICalculator(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])));
     }
     
-}
-
-int main()
-{
-    MenuSystem();
 }
 
